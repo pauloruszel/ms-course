2b97d1a0dcb222aa53abced34129220aebf7aab0
Paulo Ruszel da Luz Santos <paulo.ruszel.santos@gmail.com>
2021-01-02 16:41:34 -0300

    implementação da comunicação via RestTemplate

--- hr-payroll/src/main/java/com/pauloruszel/hrpayroll/service/PaymentService.java
+++ hr-payroll/src/main/java/com/pauloruszel/hrpayroll/service/PaymentService.java
@@ -1,12 +1,31 @@
 package com.pauloruszel.hrpayroll.service;
 
 import com.pauloruszel.hrpayroll.domain.model.Payment;
+import com.pauloruszel.hrpayroll.domain.model.Worker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class PaymentService {
 
+    @Value("${hr-worker.host}")
+    private String workerHost;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
     public Payment getPayment(long workerId, int days) {
-        return new Payment("Paulo Santos", 200.0, days);
+        Map<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("id", String.valueOf(workerId));
+        Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+        assert worker != null;
+        return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
+
+
 }
