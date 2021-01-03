89db192d253dff3660ef50884bb2cd60112f61e7
Paulo Ruszel da Luz Santos <paulo.ruszel.santos@gmail.com>
2021-01-02 22:55:38 -0300

    Projeto hr-eureka-server criado

---
+++ hr-eureka-server/src/test/java/com/pauloruszel/hreurekaserver/HrEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pauloruszel.hreurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
